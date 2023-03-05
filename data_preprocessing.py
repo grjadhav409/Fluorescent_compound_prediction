b3c2a70ffad0805058191db9e0576c7b4799e262
grjadhav409 <grjadhav409@gmail.com>
2023-03-05 20:12:16 +0530

    components added

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -20,3 +20,20 @@ def preprocess_dataframe(df):
     # df = undersampler(df)
 
     return df
+
+def preprocess_dataframe_for_regression(df):
+    # Select only SMILES and Target columns
+    df = df[['Smiles', 'Fluorescent labeling']]
+
+    # Drop duplicates
+    df = df.drop_duplicates()
+
+    # Target encoding
+    df = encode_targets(df)
+    
+
+    # Undersampling
+    
+    # df = undersampler(df)
+
+    return df
--- train_func.py
+++ train_func.py
@@ -12,9 +12,21 @@ from sklearn.neural_network import MLPClassifier
 import os
 import pandas as pd
 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.svm import SVR
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import pandas as pd
+import os
+
 # Generate some example data
 X, y = make_classification(n_samples=1000, n_features=20, random_state=42)
 PATH= "results"
+
+
 def train_models(X,y,PATH):
     # Split the data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -32,6 +44,41 @@ def train_models(X,y,PATH):
 
     # Define a list of metrics to evaluate the models
     metrics = [accuracy_score, precision_score, recall_score, f1_score, roc_auc_score]
+
+    # Create a results folder if it doesn't exist
+    if not os.path.exists(PATH):
+        os.makedirs(PATH)
+
+    # Loop through each model, train it, and evaluate its performance
+    for name, model in models.items():
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        results = {}
+        for metric in metrics:
+            metric_name = metric.__name__
+            results[metric_name] = metric(y_test, y_pred)
+        results_df = pd.DataFrame.from_dict(results, orient='index', columns=[name])
+        results_df.to_csv(f'{PATH}/{name}.csv')
+
+
+def train_regression(X,y,PATH):
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Define a dictionary of regression models
+    models = {
+        'Linear Regression': LinearRegression(),
+        'Ridge Regression': Ridge(),
+        'Lasso Regression': Lasso(),
+        'ElasticNet Regression': ElasticNet(),
+        'Decision Tree Regression': DecisionTreeRegressor(),
+        'Random Forest Regression': RandomForestRegressor(),
+        'Support Vector Regression': SVR(),
+        'K-Nearest Neighbors Regression': KNeighborsRegressor()
+    }
+
+    # Define a list of evaluation metrics to evaluate the models
+    metrics = [mean_absolute_error, mean_squared_error, r2_score]
 
     # Create a results folder if it doesn't exist
     if not os.path.exists(PATH):
